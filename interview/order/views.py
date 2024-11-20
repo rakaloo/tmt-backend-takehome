fb0a337fe020e1380ef79e8ba51190dfa7539139
Rachael Lunney <rachaellunney@Rachaels-MacBook-Pro.local>
2024-11-20 12:20:26 -0600

    Challenge 3 - Write an endpoint that lists orders that are between a particular start and embargo date

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from datetime import datetime
+
+from django.db.models import Q
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.request import Request
@@ -12,7 +14,20 @@ from interview.order.serializers import OrderSerializer, OrderTagSerializer, Ord
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        date_str = request.GET.get("between_date")
+        filter_q = Q()
+        if date_str:
+            date = datetime.strptime(date_str, '%Y-%m-%d')
+            filter_q = (
+                Q(start_date__lte=date, embargo_date__gt=date) |
+                Q(embargo_date__lte=date, start_date__gt=date)
+            )
+        serializer = self.serializer_class(self.get_queryset().filter(filter_q), many=True)
+
+        return Response(serializer.data, status=200)
+
 
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
