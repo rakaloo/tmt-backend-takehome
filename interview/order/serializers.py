87162c5142863645793d96c31e17d9991c576b11
Rachael Lunney <rachaellunney@Rachaels-MacBook-Pro.local>
2024-11-20 12:05:10 -0600

    Challenge 2 - Write a view DeactivateOrderView that sets the is_active state on the order

--- interview/order/serializers.py
+++ interview/order/serializers.py
@@ -1,7 +1,8 @@
+from django.utils import timezone
 from rest_framework import serializers
-from interview.inventory.serializers import InventorySerializer
 
 from interview.order.models import Order, OrderTag
+from interview.inventory.serializers import InventorySerializer
 
 
 class OrderTagSerializer(serializers.ModelSerializer):
@@ -18,3 +19,11 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'inventory', 'start_date', 'embargo_date', 'tags', 'is_active']
+
+
+class OrderDeactivateSerializer(OrderSerializer):
+    def update(self, instance, valiated_data):
+        instance.is_active = False
+        instance.updated_at = timezone.now()
+        instance.save()
+        return instance
--- interview/order/views.py
+++ interview/order/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
 from interview.order.models import Order, OrderTag
-from interview.order.serializers import OrderSerializer, OrderTagSerializer
+from interview.order.serializers import OrderSerializer, OrderTagSerializer, OrderDeactivateSerializer
+
 
 # Create your views here.
 class OrderListCreateView(generics.ListCreateAPIView):
@@ -13,3 +17,22 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class DeactivateOrderView(APIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderDeactivateSerializer
+
+    def patch(self, request: Request, *args, **kwargs) -> Response:
+        order = self.get_queryset(id=kwargs['id'])
+
+        serializer = self.serializer_class(order, data=request.data, partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+
+        serializer.save()
+
+        return Response(serializer.data, status=200)
+
+    def get_queryset(self, **kwargs):
+        return self.queryset.get(**kwargs)
